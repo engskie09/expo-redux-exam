15f847c9582e39062cd99c3a4791ca30ea5a4d00
engskie09 <rafaelmasallo@gmail.com>
2023-07-19 02:55:29 +0800

    ✨ feat: 📦 installed and 🏗️ implemented @ui-kitten/components

--- src/main.js
+++ src/main.js
@@ -1,14 +1,30 @@
 import { registerRootComponent } from 'expo';
 import { NavigationContainer } from '@react-navigation/native';
+import { ApplicationProvider } from '@ui-kitten/components';
+import { mapping, light, dark } from "@eva-design/eva";
 
+const theme = {
+  light: {
+    theme: light,
+    icon: "sun",
+    text: "LIGHT",
+  },
+  dark: {
+    theme: dark,
+    icon: "moon",
+    text: "DARK",
+  },
+};
 
 import Tab from './component/navigation/tab';
 
 const Main = () => {
   return (
-    <NavigationContainer>
-      <Tab/>
-    </NavigationContainer>
+    <ApplicationProvider mapping={mapping} theme={theme}>
+      <NavigationContainer>
+        <Tab/>
+      </NavigationContainer>
+    </ApplicationProvider>
   );
 }
 
