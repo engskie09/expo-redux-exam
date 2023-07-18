0e60d60356d76c377d5f9d1ce5297af5ef6d4132
engskie09 <rafaelmasallo@gmail.com>
2023-07-19 01:01:37 +0800

    🛣️ feat: ➕ added other initial screen components and 🏗️ implemented it to main navigator

--- src/component/navigation/tab.js
+++ src/component/navigation/tab.js
@@ -1,14 +1,17 @@
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import HomeScreen from '../screen/home';
+import ExpensesScreen from '../screen/expenses';
+import PortfolioScreen from '../screen/portfolio';
+import BankAccounts from '../screen/bank-accounts';
 
 const BottomTabNavigator = createBottomTabNavigator();
 
 const Tab = () => {
   return (
-    <BottomTabNavigator.Navigator initialRouteName="Home">
+    <BottomTabNavigator.Navigator initialRouteName="HOME">
       <BottomTabNavigator.Screen
-        name="Home"
+        name="HOME"
         component={HomeScreen}
         options={{
           headerShown: true,
@@ -17,7 +20,47 @@ const Tab = () => {
             backgroundColor: '#497bd0',
           },
           headerTintColor: '#fff' 
-        }} />
+        }}
+      />
+
+      <BottomTabNavigator.Screen
+        name="EXPENSES"
+        component={ExpensesScreen}
+        options={{
+          headerShown: true,
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#497bd0',
+          },
+          headerTintColor: '#fff' 
+        }}
+      />
+
+      <BottomTabNavigator.Screen
+        name="PORTFOLIO"
+        component={PortfolioScreen}
+        options={{
+          headerShown: true,
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#497bd0',
+          },
+          headerTintColor: '#fff' 
+        }}
+      />
+
+      <BottomTabNavigator.Screen
+        name="BANK_ACCOUNTS"
+        component={BankAccounts}
+        options={{
+          headerShown: true,
+          headerTitleAlign: 'center',
+          headerStyle: {
+            backgroundColor: '#497bd0',
+          },
+          headerTintColor: '#fff' 
+        }}
+      />
     </BottomTabNavigator.Navigator>
   );
 }
