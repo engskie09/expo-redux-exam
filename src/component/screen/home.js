ee11723dd190047c74d161b73cc531dfe2533985
engskie09 <rafaelmasallo@gmail.com>
2023-07-19 02:57:54 +0800

    👤 style: 🎨 refactored home base layout

--- src/component/screen/home.js
+++ src/component/screen/home.js
@@ -1,6 +1,6 @@
-import { StyleSheet, Text, View, Dimensions } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
-import { LineChart } from "react-native-chart-kit";
+import Chart from '../shared/home/chart';
 
 const styles = StyleSheet.create({
   container: {
@@ -14,38 +14,7 @@ const styles = StyleSheet.create({
 const Home = () => {
   return (
     <View style={styles.container}>
-      <LineChart
-        data={{
-          labels: ['1D', '1W', '1M', '3M', '1Y', 'All'],
-          datasets: [
-            {
-              data: [20, 45, 28, 80, 99, 43],
-              strokeWidth: 2,
-            },
-          ],
-        }}
-        width={Dimensions.get('window').width}
-        height={220}
-        chartConfig={{
-          backgroundColor: '#ffffff',
-          backgroundGradientFrom: '#ffffff',
-          backgroundGradientTo: '#ffffff ',
-          decimalPlaces: 2,
-          color: (opacity = 1) => `rgba(0, 0, 0, ${opacity})`,
-          style: {
-            borderRadius: 16,
-          },
-        }}
-        style={{
-          marginVertical: 8,
-          borderRadius: 16,
-        }}
-        withHorizontalLines={false}
-        withVerticalLines={false}
-        bezier
-        withDots={false}
-      />
-      <Text style={styles.text}>Home</Text>
+      <Chart />
     </View>
   );
 }
