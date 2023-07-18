c5d8e975882eb45f66b0e02e8e3c85710a1386d9
engskie09 <rafaelmasallo@gmail.com>
2023-07-19 02:14:46 +0800

    ✨ feat: 📦 installed and 🏗️ implemented react-native-chart-kit

--- src/component/screen/home.js
+++ src/component/screen/home.js
@@ -1,5 +1,7 @@
 import { StyleSheet, Text, View } from 'react-native';
 
+import { StackedBarChart } from "react-native-chart-kit";
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -9,9 +11,41 @@ const styles = StyleSheet.create({
   }
 });
 
+
 const Home = () => {
+  const chartConfig = {
+    backgroundGradientFrom: "#1E2923",
+    backgroundGradientFromOpacity: 0,
+    backgroundGradientTo: "#08130D",
+    backgroundGradientToOpacity: 0.5,
+    color: (opacity = 1) => `rgba(26, 255, 146, ${opacity})`,
+    strokeWidth: 2, // optional, default 3
+    barPercentage: 0.5,
+    useShadowColorFromDataset: false // optional
+  };
+
+  const data = {
+    labels: ["Test1", "Test2"],
+    legend: ["L1", "L2", "L3"],
+    data: [
+      [60, 60, 60],
+      [30, 30, 60]
+    ],
+    barColors: ["#dfe4ea", "#ced6e0", "#a4b0be"]
+  };
+
   return (  
     <View style={styles.container}>
+      <StackedBarChart
+        data={data}
+        width={200}
+        height={220}
+        chartConfig={chartConfig}
+        style={{
+          marginVertical: 8,
+          borderRadius: 16,
+        }}
+      />
       <Text style={styles.text}>Home</Text>
     </View>
   );
