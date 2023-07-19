05c7a42f1ae38bd268fd7a2901b3679d01c10e83
engskie09 <rafaelmasallo@gmail.com>
2023-07-20 00:59:10 +0800

    👤 chore: 🛠️ setup actions on mainReducer

--- src/store/reducers.js
+++ src/store/reducers.js
@@ -1,11 +1,20 @@
 const initialState = {
   photos: [],
+  videos: [],
+  details: [],
+  comments: [],
 };
 
 function mainReducer(state = initialState, action) {
   switch (action.type) {
-    case 'ACTION_HERE':
-      return { ...state };
+    case 'GET_PHOTOS':
+      return { ...state.photos, type: action.type };
+    case 'GET_VIDEOS':
+      return { ...state.videos, type: action.type };
+    case 'GET_DETAILS':
+      return { ...state.details, type: action.type };
+    case 'GET_COMMENTS':
+      return { ...state.comments, type: action.type };
     default:
       return state;
   }
